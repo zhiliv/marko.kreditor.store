8cf55551cbb97b5c539865052d94fc18e82410e7
Ivan Zhilnikov <35070562+zhiliv@users.noreply.github.com>
2019-11-28 00:20:25 +0300

    асинхронные циклы marko

--- route/main/main.js
+++ route/main/main.js
@@ -1,3 +1,13 @@
+//получение корнеовго каталога
+const appRoot = require('app-root-path');
+const query = require(`${appRoot}/lib/queryDB.js`);
+const q = require('q')
 
-  console.log(123123)
-
+exports.getData = async () => {
+  let defer = q.defer()
+ let sql = 'SELECT * FROM regions'
+ query.select(sql).then(res => {
+  defer.resolve(res.data)
+ })
+ return defer.promise;
+}
--- route/main/main.marko.js
+++ route/main/main.marko.js
@@ -10,6 +10,9 @@ var marko_template = module.exports = require("marko/src/html").t(__filename),
     marko_loadTag = marko_helpers.t,
     lasso_head_tag = marko_loadTag(require("@lasso/marko-taglib/taglib/head-tag")),
     component_globals_tag = marko_loadTag(require("marko/src/core-tags/components/component-globals-tag")),
+    marko_forEach = marko_helpers.f,
+    marko_escapeXml = marko_helpers.x,
+    await_tag = marko_loadTag(require("marko/src/core-tags/core/await/renderer")),
     lasso_body_tag = marko_loadTag(require("@lasso/marko-taglib/taglib/body-tag")),
     init_components_tag = marko_loadTag(require("marko/src/core-tags/components/init-components-tag")),
     await_reorderer_tag = marko_loadTag(require("marko/src/core-tags/core/await/reorderer-renderer"));
@@ -17,7 +20,9 @@ var marko_template = module.exports = require("marko/src/html").t(__filename),
 function render(input, out, __component, component, state) {
   var data = input;
 
-  const test = require('./main.js')
+  var list = [1, 2, 4]
+
+  var getData = require('./main.js').getData
 
   out.w("<!DOCTYPE html><html lang=\"ru\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\"><title></title>");
 
@@ -27,11 +32,29 @@ function render(input, out, __component, component, state) {
 
   component_globals_tag({}, out);
 
-  lasso_body_tag({}, out, __component, "8");
+  await_tag({
+      _provider: getData,
+      _name: "getData",
+      then: {
+          renderBody: function(out, result) {
+            var $for$0 = 0;
+
+            marko_forEach(result, function(row) {
+              var $keyScope$0 = "[" + (($for$0++) + "]");
+
+              out.w("<p>" +
+                marko_escapeXml(row.name) +
+                "</p>");
+            });
+          }
+        }
+    }, out, __component, "8");
+
+  lasso_body_tag({}, out, __component, "11");
 
   init_components_tag({}, out);
 
-  await_reorderer_tag({}, out, __component, "9");
+  await_reorderer_tag({}, out, __component, "12");
 
   out.w("</body></html>");
 }
@@ -48,6 +71,7 @@ marko_template.meta = {
     tags: [
       "@lasso/marko-taglib/taglib/head-tag",
       "marko/src/core-tags/components/component-globals-tag",
+      "marko/src/core-tags/core/await/renderer",
       "@lasso/marko-taglib/taglib/body-tag",
       "marko/src/core-tags/components/init-components-tag",
       "marko/src/core-tags/core/await/reorderer-renderer"
